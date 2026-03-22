2685de0541dde7d90533916c77784c670980cba9
Wellington Amorim <amorimwellington788@gmail.com>
2026-03-22 11:33:34 -0300

    model criado

---
+++ src/main/java/com/as/gerenciamento_categorias/models/CategoriaModel.java
@@ -0,0 +1,42 @@
+package com.as.gerenciamento_categorias.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TBL_CATEGORIA")
+public class CategoriaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome_categoria")
+    private String nome;
+    @Column(name = "descricao_categoria")
+    private String descricao;
+
+    public CategoriaModel() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
